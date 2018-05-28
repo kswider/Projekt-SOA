8114b3281725a12297f2e653cd84a8c35d997c12
Krzysiek <vzxc@student.agh.edu.pl>
2018-05-28 22:22:56 +0200

    Initial commit - basic REST server with 2 controllers.

---
+++ Toulbar2RestCore/Toulbar2RestCore/Controllers/ExampleController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Toulbar2RestCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("/Example")]
+    public class ExampleController : Controller
+    {
+        // GET: api/Example
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Example/5
+        public string Get(int id)
+        {
+            //string directoryPath = @"C:\Users\Krzysiek\Desktop\resttest\";
+            string directoryPath = @"/usr/bin/";
+            //            string trailer;
+            //           if (id == 2)
+            //             trailer = ".wcnf";
+            //          else
+            string trailer = ".wcsp";
+            StringBuilder output = new StringBuilder();
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = $"{directoryPath}toulbar2";
+                process.StartInfo.Arguments = $"-w=\"{directoryPath}\\examples\\{id}.res\" {directoryPath}\\examples\\{id}{trailer}";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                //process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.Verb = "runas";
+                process.Start();
+                process.WaitForExit();
+                string error = process.StandardError.ReadToEnd();
+                output.Append(process.StandardOutput.ReadToEnd());
+                process.Close();
+                if (System.IO.File.Exists($@"{directoryPath}\examples\{id}.res"))
+                {
+                    output.Append("Rozwiązanie problemu:\n");
+                    output.Append(System.IO.File.ReadAllText($@"{directoryPath}\examples\{id}.res"));
+                    System.IO.File.Delete($@"{directoryPath}\examples\{id}.res");
+                }
+                else
+                {
+                    output.Append("Nie znaleziono rozwiązania dla danego problemu!");
+                }
+            }
+            catch (Exception e)
+            {
+                output.Append("\nWystąpił błąd! Czy na pewno wysłałeś poprawny plik? Logi:\n");
+                output.Append(e.StackTrace);
+                Console.Out.WriteLine(e.StackTrace);
+            }
+
+            return output.ToString();
+        }
+
+
+        // POST: api/Example
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Example/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Toulbar2RestCore/Toulbar2RestCore/Controllers/FileController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Toulbar2RestCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("Toulbar2REST/File")]
+    public class FileController : Controller
+    {
+        // GET: api/File
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/File/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/File
+        public string Post([FromBody]string value)
+        {
+            //string directoryPath = @"C:\Users\Krzysiek\Desktop\resttest\";
+            string directoryPath = @"/usr/bin/";
+            Random random = new Random();
+            string fileFullPath = $"{directoryPath}{random.Next(10000)}tmp.wcsp";
+            System.IO.File.WriteAllText(fileFullPath, value);
+
+            StringBuilder output = new StringBuilder();
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = $"{directoryPath}toulbar2";
+                process.StartInfo.Arguments = $"-w=\"{fileFullPath}sol\" {fileFullPath}";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                //process.StartInfo.WorkingDirectory = @"C:\Program Files (x86)\toulbar2 0.9.8\bin\";
+                //process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.Verb = "runas";
+                process.Start();
+                process.WaitForExit();
+                output.Append("Alokacja pamięci dla zmiennych:\n");
+                output.Append(process.StandardOutput.ReadToEnd());
+                string error = process.StandardError.ReadToEnd();
+                process.Close();
+                if (System.IO.File.Exists($"{fileFullPath}sol"))
+                {
+                    output.Append("Rozwiązanie problemu:\n");
+                    output.Append(System.IO.File.ReadAllText($"{fileFullPath}sol"));
+                    System.IO.File.Delete($"{fileFullPath}sol");
+                }
+                else
+                {
+                    output.Append("Nie znaleziono rozwiązania dla danego problemu!");
+                }
+                System.IO.File.Delete(fileFullPath);
+
+            }
+            catch (Exception e)
+            {
+                output.Append("\nWystąpił błąd! Czy na pewno wysłałeś poprawny plik? Logi:\n");
+                output.Append(e.StackTrace);
+                Console.Out.WriteLine(e.StackTrace);
+            }
+
+            return output.ToString();
+        }
+
+        // PUT: api/File/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
