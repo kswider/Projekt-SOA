8a347febde50d08d3da7c272f426ed7cff29e4e4
Krzysiek <vzxc@student.agh.edu.pl>
2018-06-11 14:40:37 +0200

    Minor changes in razor page

--- Toulbar2RestCore/Toulbar2RestCore/Pages/Index.cshtml.cs
+++ Toulbar2RestCore/Toulbar2RestCore/Pages/Index.cshtml.cs
@@ -9,9 +9,9 @@ namespace Toulbar2RestCore.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return Page();
         }
     }
 }
